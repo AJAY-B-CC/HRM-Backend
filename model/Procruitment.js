0fa62b48affc57f5915666436e9f33dccd132270
AjayB101 <ajayjadec@gmail.com>
2023-12-13 21:24:05 +0530

    backend chnged

--- model/Procruitment.js
+++ model/Procruitment.js
@@ -1,62 +1,64 @@
 const mongoose = require("mongoose");
 
 const procruitmentSchema = new mongoose.Schema({
-	productname: {
-		type: String,
-	},
-	Specification: {
-		type: String,
-	},
-	businessJustification: {
-		type: String,
-	},
-	quantity: {
-		type: Number,
-	},
-	approximateBudget: {
-		type: String,
-	},
-	priority: {
-		type: String,
-		default: "Low",
-	},
-	status: {
-		type: String,
-		default: "Pending",
-	},
-	productLink: {
-		type: String,
-	},
-	vendorName: {
-		type: String,
-	},
-	vendorNumber: {
-		type: Number,
-	},
-	employeeid: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: "employee",
-	},
-	attachments: {
-		public_id: {
-			type: String,
-		},
-		url: {
-			type: String,
-		},
-	}, 
-	reportingTo: [ 
-		{
-			employee: {
-			  type: mongoose.Schema.Types.ObjectId,
-			  ref: 'employee',
-			},
-			approved: {
-				type: Boolean,
-				default: false,
-			},
-		},
-	],
-},{timestamps:true});
+    productname: {
+        type: String,
+    },
+    Specification: {
+        type: String,
+    },
+    businessJustification: {
+        type: String,
+    },
+    quantity: {
+        type: Number,
+    },
+    approximateBudget: {
+        type: String,
+    },
+    priority: {
+        type: String,
+        default: "Low",
+    },
+    status: {
+        type: String,
+        default: "Pending",
+    },
+    productLink: {
+        type: String,
+    },
+    vendorName: {
+        type: String,
+    },
+    vendorNumber: {
+        type: Number,
+    },
+    employeeid: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "employee",
+    },
+    attachments: {
+        public_id: {
+            type: String,
+            required: false,
+        },
+        url: {
+            type: String,
+            required: false,
+        },
+    },
+    reportingTo: [
+        {
+            employee: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'employee',
+            },
+            approved: {
+                type: Boolean,
+                default: false,
+            },
+        },
+    ],
+}, { timestamps: true });
 
 module.exports = mongoose.model("procruitment", procruitmentSchema);
